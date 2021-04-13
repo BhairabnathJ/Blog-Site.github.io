fd1d7e1dbcc1de9771b210b85c160c90fda56e94
Alex Jamkatel <clickbaitviewer@gmail.com>
2021-04-13 17:34:18 -0400

    APP.JS ADD #1 but THE ERRORS ARE FUCKING ANNOYING!

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,6 @@
-console.log('Hello!')
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListner('submit', (e)=>{
+    e.preventDefault();
+    console.log("submit clicked");
+});
