91d73a076202663bce41df0732bfb7fbd6ff2b6d
Alex Jamkatel <clickbaitviewer@gmail.com>
2021-04-13 16:52:32 -0400

    Changed DIR, added sub btn

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
-    res.send('Hello!')
+    res.sendFile(__dirname + '/public/contactform.html')
 })
 
 app.listen(PORT, ()=>{
